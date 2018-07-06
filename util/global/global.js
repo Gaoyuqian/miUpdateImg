59f936b1dcf1eebede44af21e6b28e00c603bed6
高毓谦 <yuqian.gao@mljr.com>
2018-07-06 14:51:07 +0800

    bugfix

--- util/global/global.js
+++ util/global/global.js
@@ -1,19 +1,19 @@
 // 定义一些伪全局变量 防止循环引用
-const __config = {
-    'miuiFile.json':{
-        "fileUpdatePath": ["./publish","./src"],
-        "output": "uploadPackage.json",
-        "ignored": "./ignored",
-        "httpsOption": {
-            "hostname": "file.market.miui.srv",
-            "port": 8756,
-            "path": "/upload?channel=NccFgber",
-            "method": "POST"
-        },
-        "fileFindPath": "./src"
-    },
-    'uploadPackage.json':{}
+const _config = {
+  'miuiFile.json':{
+      "fileUpdatePath": ["./publish","./src"],
+      "output": "uploadPackage.json",
+      "ignored": "./ignored",
+      "httpsOption": {
+          "hostname": "file.market.miui.srv",
+          "port": 8756,
+          "path": "/upload?channel=NccFgber",
+          "method": "POST"
+      },
+      "fileFindPath": "./src"
+  },
+  'uploadPackage.json':{}
 }
 module.exports = {
-  __config
+_config
 }
